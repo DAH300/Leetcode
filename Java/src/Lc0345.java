2dc235ece1521b9794bb4cdc4380ab056a61c53c
DAH300 <DHowell300@Comcast.net>
2025-02-18 00:18:29 -0500

    Update Lc0345.java
    345. Reverse Vowels of a String

--- Java/src/Lc0345.java
+++ Java/src/Lc0345.java
@@ -3,9 +3,27 @@ public class Lc0345 {
     public static class Solution {
         public String reverseVowels(String s) {
 
-            
+            char[] chars = s.toCharArray();
+            String vowels = "AEIOUaeiou";
+            int pointer1 = 0;
+            int pointer2 = s.length() - 1;
 
-            return s;
+            while (pointer1 < pointer2) {
+                if (vowels.indexOf(chars[pointer1]) != -1) {
+                    if (vowels.indexOf(chars[pointer2]) != -1) {
+                        char temp = chars[pointer1];
+                        chars[pointer1] = chars[pointer2];
+                        chars[pointer2] = temp;
+                        pointer1++;
+                        pointer2--;
+                    } else {
+                        pointer2--;
+                    }
+                } else {
+                    pointer1++;
+                }
+            }
+            return new String(chars);
         }
     }
 
@@ -21,13 +39,12 @@ public class Lc0345 {
         // Example 2:
         // Input: s = "leetcode"
         // Output: "leotcede"
-        
+
         Solution sol = new Solution();
 
         String s1 = "IceCreAm";
         String s2 = "leetcode";
 
-        System.out.println(sol.reverseVowels(s1));
+        System.out.println(sol.reverseVowels(s2));
     }
-
 }
